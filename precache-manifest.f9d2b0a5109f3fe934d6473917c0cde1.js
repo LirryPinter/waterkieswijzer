16b78eb1807534d82fd5e721f5fd8422a852c0b7
Lirry Pinter <lirry.pinter@nelen-schuurmans.nl>
2019-02-19 13:27:26 +0100

    Updates

---
+++ precache-manifest.f9d2b0a5109f3fe934d6473917c0cde1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ca03d4c1c07f4742f3e58e84c469c49",
+    "url": "/waterkieswijzer/static/media/de-waag.2ca03d4c.svg"
+  },
+  {
+    "revision": "006cd2856e603332a585",
+    "url": "/waterkieswijzer/static/js/runtime~main.006cd285.js"
+  },
+  {
+    "revision": "03ea3cdc7ad55888ae92",
+    "url": "/waterkieswijzer/static/js/main.03ea3cdc.chunk.js"
+  },
+  {
+    "revision": "6be8e46bfb534592d6ec",
+    "url": "/waterkieswijzer/static/js/1.6be8e46b.chunk.js"
+  },
+  {
+    "revision": "03ea3cdc7ad55888ae92",
+    "url": "/waterkieswijzer/static/css/main.9d8e704e.chunk.css"
+  },
+  {
+    "revision": "3241d83c01ae6029ceabaa4662a02d49",
+    "url": "/waterkieswijzer/index.html"
+  }
+];
